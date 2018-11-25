619b9dae188c6d120464e22fd9001843303c69f7
gasparagus <gary2g99@gmail.com>
2018-11-25 18:00:57 -0500

    working code section 4

--- code/app_4_50.py
+++ code/app_4_50.py
@@ -7,24 +7,26 @@ api = Api(app)
 
 items = []
 
+
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        return {'item': (filter(lambda x: x['name'] == name, items), None)}
 
     def post(self, name):
+        if next(iter(filter(lambda x: x['name'] == name, items)), None) is not None:
+            return {'message': "An item with name '{}' already exists.".format(name)}
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
+
 class ItemList(Resource):
     def get(self):
         return {'items': items}
 
+
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
-app.run(port=5000, debug=True)
+app.run(port=5000)
